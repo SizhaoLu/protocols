0c530001516c49b2bdb5d6a7e79affd833e4f53a
Sizhao Lu <sizhao.lu@gmail.com>
2026-06-19 22:58:25 -0600

    add code snippets

--- publish.el
+++ publish.el
@@ -2,27 +2,55 @@
 
 (setq org-publish-project-alist
       '(("protocols"
-         :base-directory "org"
+         :base-directory "org/protocols"
          :base-extension "org"
-         :publishing-directory "public"
+         :publishing-directory "public/protocols"
          :recursive t
          :publishing-function org-html-publish-to-html
          :with-toc t
          :section-numbers nil
-         :html-head "<link rel=\"stylesheet\" href=\"style.css\" type=\"text/css\"/>"
+         :html-head "<link rel=\"stylesheet\" href=\"../style.css\" type=\"text/css\"/>"
          :html-validation-link nil
          :html-postamble "<p>Last built: %T</p>"
          :auto-sitemap t
          :sitemap-filename "index.org"
-         :sitemap-title "Experimental Protocols")
+         :sitemap-title "Protocols")
 
-        ("protocols-static"
+        ("snippets"
+         :base-directory "org/snippets"
+         :base-extension "org"
+         :publishing-directory "public/snippets"
+         :recursive t
+         :publishing-function org-html-publish-to-html
+         :with-toc t
+         :section-numbers nil
+         :html-head "<link rel=\"stylesheet\" href=\"../style.css\" type=\"text/css\"/>"
+         :html-validation-link nil
+         :html-postamble "<p>Last built: %T</p>"
+         :auto-sitemap t
+         :sitemap-filename "index.org"
+         :sitemap-title "Code Snippets")
+
+        ("home"
          :base-directory "org"
-         :base-extension "css\\|png\\|jpg\\|gif\\|pdf"
+         :base-extension "org"
+         :exclude ".*"
+         :include ("index.org")
+         :publishing-directory "public"
+         :publishing-function org-html-publish-to-html
+         :with-toc nil
+         :section-numbers nil
+         :html-head "<link rel=\"stylesheet\" href=\"style.css\" type=\"text/css\"/>"
+         :html-validation-link nil
+         :html-postamble "<p>Last built: %T</p>")
+
+        ("static"
+         :base-directory "org"
+         :base-extension "css\\|png\\|jpg\\|jpeg\\|gif\\|svg\\|webp\\|pdf"
          :publishing-directory "public"
          :recursive t
          :publishing-function org-publish-attachment)
 
-        ("all" :components ("protocols" "protocols-static"))))
+        ("all" :components ("home" "protocols" "snippets" "static"))))
 
 (org-publish-project "all" t)
